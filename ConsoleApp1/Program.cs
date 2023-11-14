d6b715879ff762509353ad1f47809a414f894a1d
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-11-13 21:54:21 -0300

    avanzando con logica

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -54,8 +54,82 @@ Piece FrenteLlaves = new()
 Operation? LHOp30 = new() { Number = 3, Duration = 10, HasPreOperations = false, HasPostOperations = true };
 Operation? LHOp40 = new() { Number = 4, Duration = 12, HasPreOperations = true, HasPostOperations = true };
 Operation? LHOp50 = new() { Number = 5, Duration = 7, HasPreOperations = true, HasPostOperations = false };
+Piece LateralHorno = new()
+{
+    Name = "Lateral Horno",
+    Operations = [LHOp30, LHOp40, LHOp50]
+};
 
+List<Piece> PieceList = new() { PlanchaBlanca, RespaldoBlanco, FrenteLlaves };
+List<Machine> MachineList = new() { Smp41, Smp42, Smp43 };
 
+foreach (var piece in PieceList)
+{
+    for (int i = 0; i < piece.Operations?.Length; i++)
+    {
+        int availableStartTime = 0;
+        string? availableMachine = null;
+        int availableWorker = 0;
+        int preOperationFinishTime = piece.Operations[i].HasPreOperations ? piece.Operations[i - 1].EndTime : 0; 
+        // Check for an available machine time block 
+        foreach (var machine in MachineList)
+        {
+            int machineAvailableStartTime = 0;
+            string? machineAvailableName = null;
+            
+            for (int j = preOperationFinishTime; j < machine.AvailableTime?.Length; j++)
+            {
+                for (int k = 0; k < piece.Operations[i].Duration;  k++)
+                {
+                    if (machine.AvailableTime[j] != null)
+                        break;
+                    if (k == piece.Operations[i].Duration - 1)
+                    {
+                        if (j <= machineAvailableStartTime)
+                        { 
+                            machineAvailableStartTime = j;
+                            machineAvailableName = machine.Name;
+                        }
+                    } 
+                }
+
+                for (int m = 0; m < availableWorkers.Length; m++ )
+                {
+                    for (int l = 0; l < availableWorkers[m].Length; l++)
+                    {
+                        for (int o = 0; o < piece.Operations[i].Duration; o++)
+                        {
+                            if (availableWorkers[m][o] != null)
+                                break;
+                            if (o == piece.Operations[i].Duration - 1)
+                            {
+                                if (l == machineAvailableStartTime)
+                                {
+                                    availableWorker = m;
+                                    availableStartTime = l;
+                                    availableMachine = machineAvailableName;
+                                    piece.Operations[i].StartTime = availableStartTime;
+                                    piece.Operations[i].EndTime = availableStartTime + piece.Operations[i].Duration;
+                                }
+                            } 
+
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine("=====");
+        Console.WriteLine($"Pieza: {piece.Name}");
+        Console.WriteLine($"Operación: {i}");
+        Console.WriteLine($"Maquina: {availableMachine}");
+        Console.WriteLine($"Operario: {availableWorker}");
+        Console.WriteLine($"Hora inicio: {piece.Operations[i].StartTime}");
+        Console.WriteLine($"Hora fin: {piece.Operations[i].EndTime}");
+        Console.WriteLine("=====");
+        // Check for an available worker time block
+        // Assign operation
+    }
+}
 
 public class Machine
 {
@@ -75,6 +149,8 @@ public class Operation
     public int Duration { get; set; }
     public bool HasPreOperations { get; set; }
     public bool HasPostOperations { get; set; }
+    public int StartTime = 0;
+    public int EndTime = 0;
 }
 
 /*
