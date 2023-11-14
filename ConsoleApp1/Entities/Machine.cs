ee64fa98152cf6b5e439e2adaf92f481d0c2c650
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-11-13 22:40:51 -0300

    refactoring

---
+++ ConsoleApp1/Entities/Machine.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1.Entities
+{
+    public class Machine(int lotDuration, string name)
+    {
+        public string Name { get; set; } = name;
+        public string[]? AvailableTime { get; set; } = new string[lotDuration];
+    }
+}
---
+++ ConsoleApp1/Entities/Piece.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1.Entities
+{
+    public class Piece(string? name, Operation[] operations)
+    {
+        public string? Name { get; set; } = name;
+        public Operation[] Operations { get; set; } = operations;
+    }
+}
